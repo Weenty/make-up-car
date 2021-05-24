aeb5a929501fcb679cb3612173c5dbd42b80868d
Mr-Kp9lKHyTbli <shura.makyan@gmail.com>
2021-05-24 14:34:32 +0500

    низ

--- make up/car/src/App.vue
+++ make up/car/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <banner>  </banner>
+    <fotter>  </fotter>
+
     </div>
 </template>
 
 <script>
 import banner from './components/banner';
+import fotter from './components/fotter';
 export default {
   name: 'App',
 
   components: {
     banner,
+    fotter,
+    
   },
 
   data: () => ({
---
+++ make up/car/src/components/fotter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+jghh
+  </div>
+</template>
